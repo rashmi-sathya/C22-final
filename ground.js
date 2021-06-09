146f848cf21e6f83cf18cce45803a17fa42a5d49
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-06-09 17:21:47 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,27 +1,25 @@
-class Ground
+class Ground 
+{
+  constructor(x, y, w, h) 
   {
-    constructor(x,y,w,h)
-    {
-        var ground_options ={
-            isStatic: true
-          };
-      this.x =x;
-      this.y = y;
-      this.w = w;
-      this.h = h;
-      this.body = Bodies.rectangle(x,y,w,h,ground_options);
-      World.add(world,this.body);
-    }
-  
-    show()
-    {
-       
-        rectMode(CENTER);
-        stroke(255);
-        fill(127);
-        rect(this.x, this.y, this.w, this.h);
-     
-      }
-
-
+    let options = {
+      isStatic:true
+    };
+    
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
   }
+
+  show() {
+    var pos = this.body.position;
+    push();
+    rectMode(CENTER);
+    stroke(255);
+    fill(127);
+    rect(pos.x, pos.y, this.w, this.h);
+    pop();
+  }
+  
+}
